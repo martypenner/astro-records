9355eaf0442919c8fd0b14b5c4d457e84b78183d
Marty Penner <martypenner@users.noreply.github.com>
2024-03-11 18:00:34 -0400

    feat: add log of podcast feeds in root

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
+import process from 'node:process';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.dev.vars',
+});
 
 const DEV_PARTYKIT_HOST = '127.0.0.1:1999';
 
@@ -14,6 +20,12 @@ export default defineConfig({
   vite: {
     define: {
       'import.meta.env.PARTYKIT_HOST': JSON.stringify(DEV_PARTYKIT_HOST),
+      'import.meta.env.PUBLIC_PODCAST_INDEX_API_KEY': JSON.stringify(
+        import.meta.env.PUBLIC_PODCAST_INDEX_API_KEY,
+      ),
+      'import.meta.env.PUBLIC_PODCAST_INDEX_API_SECRET': JSON.stringify(
+        import.meta.env.PUBLIC_PODCAST_INDEX_API_SECRET,
+      ),
     },
   },
 });
