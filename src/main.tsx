417dce28af903c45c246a94aa82dae14ecd050ad
Marty Penner <martypenner@users.noreply.github.com>
2024-03-22 16:59:48 -0400

    fix: ensure we only have one reflect server running in dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Component as Root } from './routes/Root';
 import ErrorPage from './error-page';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { r } from './reflect';
 
 import './styles/tailwind.css';
 import './styles/transitions.css';
@@ -25,8 +26,17 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(async () => {
+    // this makes sure that there is only one instance of the reflect client during hmr reloads
+    await r.close();
+    root.unmount();
+  });
+}
